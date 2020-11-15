1f1dd055146fd69f73444856b45f3791946c62f6
wRWebDev <wrobertson@hd9host.com>
2020-11-15 22:30:56 +0000

    Changed homepage title

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Link from 'next/link'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <main>
-      <h1>RELAY FOR LIFE</h1>
-      <Link href="/booking/payment"><a>Donate now</a></Link>
-    </main>
+    <Layout title="Relay For Life">
+      <main>
+        <h1>RELAY FOR LIFE</h1>
+        <Link href="/booking/payment"><a>Donate now</a></Link>
+      </main>
+    </Layout>
   )
 }
